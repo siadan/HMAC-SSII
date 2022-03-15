5877edaa7d04ab872081d61d3b64d03051d72518
jorruirui1 <jorruirui1@alum.us.es>
2022-03-15 11:12:24 +0100

    AHORA COMPRUEBA SI LOS HASHES SON CORRECTOS

--- src/clientsocket.py
+++ src/clientsocket.py
@@ -7,7 +7,7 @@ import hashlib
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 3030  # The port used by the server
 key = "caca"
-msg = "hola"
+msg = "hola servidor"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
--- src/serversocket.py
+++ src/serversocket.py
@@ -21,9 +21,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             msg = received[0]
             digest = received[1]
             hmac1 = hmac.digest(key=key.encode(), msg=msg.encode(), digest="sha3_256")
-            if hmac.compare_digest(hmac1, digest):
+            if hmac.compare_digest(str(hmac1), digest):
                 print("No ha habido fallos en la integridad")
             else:
                 print("Ha habido fallos en la integridad")
-            print(f"Received {data!r}")
+            print(f"Received {msg!r}")
             conn.sendall(data)
