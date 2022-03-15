24add3b48074254901be28cb296e89cf13472f40
jorruirui1 <jorruirui1@alum.us.es>
2022-03-15 10:26:34 +0100

    Implementación con hmac

--- src/clientsocket.py
+++ src/clientsocket.py
@@ -1,13 +1,21 @@
 # clientsocket.py
 
 import socket
+import hmac
+import hashlib
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 3030  # The port used by the server
+key = "caca"
+msg = "hola"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
+    hmac1 = hmac.digest(key=key.encode(), msg=msg.encode(), digest="sha3_256")
+
+    s.sendall((msg + "\n" + str(hmac1)).encode("utf-8"))
+    
     data = s.recv(1024)
 
-print(f"Received {data!r}")
+
+print(f"Sent {data!r}")
--- src/serversocket.py
+++ src/serversocket.py
@@ -1,9 +1,11 @@
 # serversocket.py
 
 import socket
+import hmac
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 3030  # Port to listen on (non-privileged ports are > 1023)
+key = "caca"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -15,4 +17,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
+            received = data.decode("utf-8").split("\n")
+            msg = received[0]
+            digest = received[1]
+            hmac1 = hmac.digest(key=key.encode(), msg=msg.encode(), digest="sha3_256")
+            if hmac.compare_digest(hmac1, digest):
+                print("No ha habido fallos en la integridad")
+            else:
+                print("Ha habido fallos en la integridad")
+            print(f"Received {data!r}")
             conn.sendall(data)
